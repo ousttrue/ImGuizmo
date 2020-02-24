d67a6dcc15105a91746990ba5e020175247d9514
ousttrue <ousttrue@gmail.com>
2020-02-25 01:40:58 +0900

    ImGui_Impl_Win32_UpdateMouseCursor to main.cpp

--- example/ImApp.cpp
+++ example/ImApp.cpp
@@ -7,57 +7,6 @@
 #include <imgui.h>
 
 
-static void ImGui_Impl_Win32_UpdateMouseCursor()
-{
-	ImGuiIO &io = ImGui::GetIO();
-	if (io.ConfigFlags & ImGuiConfigFlags_NoMouseCursorChange)
-	{
-		SetCursor(LoadCursor(NULL, IDC_ARROW));
-		return;
-	}
-
-	ImGuiMouseCursor imgui_cursor = ImGui::GetMouseCursor();
-	if (imgui_cursor == ImGuiMouseCursor_None || io.MouseDrawCursor)
-	{
-		// Hide OS mouse cursor if imgui is drawing it or if it wants no cursor
-		::SetCursor(NULL);
-		return;
-	}
-
-	// Show OS mouse cursor
-	LPTSTR win32_cursor = IDC_ARROW;
-	switch (imgui_cursor)
-	{
-	case ImGuiMouseCursor_Arrow:
-		win32_cursor = IDC_ARROW;
-		break;
-	case ImGuiMouseCursor_TextInput:
-		win32_cursor = IDC_IBEAM;
-		break;
-	case ImGuiMouseCursor_ResizeAll:
-		win32_cursor = IDC_SIZEALL;
-		break;
-	case ImGuiMouseCursor_ResizeEW:
-		win32_cursor = IDC_SIZEWE;
-		break;
-	case ImGuiMouseCursor_ResizeNS:
-		win32_cursor = IDC_SIZENS;
-		break;
-	case ImGuiMouseCursor_ResizeNESW:
-		win32_cursor = IDC_SIZENESW;
-		break;
-	case ImGuiMouseCursor_ResizeNWSE:
-		win32_cursor = IDC_SIZENWSE;
-		break;
-	case ImGuiMouseCursor_Hand:
-		win32_cursor = IDC_HAND;
-		break;
-		// case ImGuiMouseCursor_NotAllowed:
-		//     win32_cursor = IDC_NO;
-		//     break;
-	}
-	::SetCursor(::LoadCursor(NULL, win32_cursor));
-}
 
 
 class Impl
@@ -86,8 +35,6 @@ public:
 			return false;
 		}
 
-		return (1);
-
 		if (!::InitExtension())
 		{
 			return false;
@@ -110,7 +57,6 @@ public:
 
 		// Start the frame
 		ImGui_Impl_ScreenState_NewFrame(state);
-		ImGui_Impl_Win32_UpdateMouseCursor();
 		ImGui::NewFrame();
 
 		return true;
